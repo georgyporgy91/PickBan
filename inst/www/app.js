1f83b9a4089f81641f80054ca2d505b2b90bf36f
George Wang <gwang@ondeck.com>
2016-12-11 23:14:35 -0500

    update html and javascript

--- inst/www/app.js
+++ inst/www/app.js
@@ -1,18 +1,18 @@
-//set CORS to call "tvscore" package on public server
-//ocpu.seturl("//public.opencpu.org/ocpu/github/gwang16/PickBan/R")
 
-//some example data
-//to run with different data, edit and press Run at the top of the page
-var mydata = [
-  {friendly : "Thresh", opponent: "Tristana"}
-];
+	//set CORS to call "tvscore" package on public server
+	//ocpu.seturl("public.opencpu.org/ocpu/github/gwang16/PickBan/R")
 
-//call R function: tvscore::tv(input=data)
-$("#submitbutton").click(function(){
-    
-  var req = ocpu.rpc("counterpick",{
-    input : mydata
-  }, function(output){
+	//some example data
+	//to run with different data, edit and press Run at the top of the page
+$(document).ready(function() {
+	$("#submitbutton").click(function(e){
+		e.preventDefault()
+		
+		var mydata = [
+	  {friendly : "Thresh", opponent: "Tristana"}
+		];
+		
+		var req = ocpu.rpc("counterpick",{input : mydata}, function(output){
     $("tbody").empty();
     $.each(output, function(index, value){
       var html = "<tr><td>" + value.champion + "</td><td>" + value.pwin + "</td><td>" + value.numGames + "</td></tr>";
@@ -24,4 +24,6 @@ $("#submitbutton").click(function(){
   req.fail(function(){
     alert("R returned an error: " + req.responseText); 
   });
+
+});
 });
