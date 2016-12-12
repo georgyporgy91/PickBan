154e5a028b1382d46cf2f755e276556be7f10fdb
George Wang <gwang@ondeck.com>
2016-12-12 00:03:29 -0500

    update buttons

--- inst/www/app.js
+++ inst/www/app.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 	$("#submitbutton").click(function(e){
 		e.preventDefault()
 		
-		var b1 = document.getElementById('b1');
-		var p1 = document.getElementById('p1');
+		var b1 = document.getElementById('b1').value;
+		var p1 = document.getElementById('p1').value;
 		var mydata = [{friendly : b1, opponent: p1}];
 
-		var req = ocpu.rpc("counterpick",{input : mydata}, function(output){
+		var req = ocpu.rpc("counterpick", {input : mydata}, function(output){
 	    	$("tbody").empty();
 	    	$.each(output, function(index, value){
 	      		var html = "<tr><td>" + value.champion + "</td><td>" + value.pwin + "</td><td>" + value.numGames + "</td></tr>";
