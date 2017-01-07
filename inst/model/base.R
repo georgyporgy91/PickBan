1dedb1d6dad9df0fc3e755b72d67386997e3723f
George Wang <gwang@ondeck.com>
2017-01-07 16:42:20 -0500

    update html

--- inst/model/base.R
+++ inst/model/base.R
@@ -5,17 +5,17 @@ library(data.table)
 library(jsonlite)
 
 ## read data as dat with columns champion, gameid, teamid, win/lose, pickid (optional)
-raw_dat <- read.csv("ignore/RequestData_pre_ranked.csv")
+raw_dat <- read.csv("~/Desktop/PickBan/inst/model/RequestData_pre_ranked.csv")
 dat <- with(raw_dat, data.frame(gameid=matchId, champion, teamid= side, result = winner))
 dat$teamid <- replace(dat$teamid, dat$teamid==100, 1)
 dat$teamid <- replace(dat$teamid, dat$teamid==200, 2)
 dat$result <- replace(dat$result, dat$result==TRUE, "w")
 dat$result <- replace(dat$result, dat$result==FALSE, "l")
 
-championMapping <- read.csv("ignore/championMapping.csv", row.names = 1)
+championMapping <- read.csv("~/Desktop/PickBan/inst/model/championMapping.csv", row.names = 1)
 dat$champion <- left_join(dat, championMapping[,1:2], by= "champion")$"championNames"
 
-champion_roles_df <- read.csv("ignore/champion_roles_df.csv")
+champion_roles_df <- read.csv("~/Desktop/PickBan/inst/model/champion_roles_df.csv")
 champion_roles_df$roles_combined <- with(champion_roles_df, paste0(role1, "_",role2))
 
 championMapping <- left_join(championMapping, champion_roles_df, by="championNames") #join roles to champion table
@@ -37,7 +37,7 @@ highestWinProb <- function(dat, n){
   threshold <- sum(win_rate_sorted$numGames)/length(win_rate_sorted$champion) #expected # of games per champion, if randomly selected
   #threshold <- 0
   win_rate_clean <- win_rate_sorted[win_rate_sorted$numGames >=threshold,]
-  return(win_rate_clean)  
+  return(win_rate_clean)
   # print(win_rate_clean)
   # highestWinRatePick <- win_rate_clean$champion[1:n]
   # highestWinRatePick_rate <- win_rate_clean$pwin[1:n]
@@ -54,13 +54,14 @@ firstRoundPick <- function(dat){
 highestWinProb_group <- function(dat, n){
   dat <- data.table(dat)
   dat$champion <- as.character(dat$champion)
-  dat <- merge(dat_temp, championMapping[,c("championNames","roles_combined")], by.x = "champion", by.y = "championNames", all.x = TRUE)
+  dat <- merge(dat, championMapping[,c("championNames","roles_combined")], by.x = "champion", by.y = "championNames", all.x = TRUE)
 
   # SELECT pwin and count, BY champion, WHERE count > (total number of games / number of champions), ORDER BY pwin desc order
   win_rate_clean <- dat[ , list(pwin= sum(result=="w")/.N ,.N), champion][N> (sum(N)/length(N)),][order(-pwin)]
   #win_rate_clean <- dat[ , list(pwin= sum(result=="w")/.N ,.N), roles_combined][N> (sum(N)/length(N)),][order(-pwin)]
-  highestWinRatePick <- win_rate_clean$champion[1:n]
-  return(highestWinRatePick)
+  return(win_rate_clean)
+  # highestWinRatePick <- win_rate_clean$champion[1:n]
+  # return(highestWinRatePick)
 }
 
 
